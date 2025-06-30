9293e50eec7f6a2230729e08e0c1afbc37a16628
Giuliano99 <giuliano.gaub@tum.de>
2025-06-30 16:18:54 +0200

    Refactor init_ge.py to improve exception handling during suite retrieval

--- darts_project/scripts/validation/init_ge.py
+++ darts_project/scripts/validation/init_ge.py
@@ -1,5 +1,4 @@
 from great_expectations import get_context
-from great_expectations.exceptions import SuiteNotFoundError
 
 def init_darts_results_suite():
     GE_ROOT_DIR = "/home/pi/airflow/dags/darts_project/great_expectations"
@@ -8,10 +7,9 @@ def init_darts_results_suite():
     context = get_context(project_root_dir=GE_ROOT_DIR)
 
     try:
-        # This is the correct way to get a suite in v1.5.1
         context.suites.get(suite_name)
         print(f"Suite '{suite_name}' already exists.")
-    except SuiteNotFoundError:
+    except Exception:
         suite = context.suites.create(suite_name)
         context.suites.save(suite)
         print(f"✅ Created new expectation suite '{suite_name}'")
